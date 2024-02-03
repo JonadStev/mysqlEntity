c86557c6fb752a2d8876a1a96035c79f13ec2b69
JonadStev <stevenjonathan1995@gmail.com>
2024-02-03 12:03:38 -0500

    first commit

---
+++ src/test/java/com/dao/mysql/MysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dao.mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
